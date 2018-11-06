65b194c49984c239808ab001676cb04f925ea58d
Pasquale Piscitelli <pasjp6@gmail.com>
2018-11-06 18:24:10 -0500

    Initial commit

--- server/server.js
+++ server/server.js
@@ -42,3 +42,17 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+
+
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+var loopbackSSL = require('loopback-ssl');
+ 
+var app = module.exports = loopback();
+ 
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+});
+ 
+return loopbackSSL.startServer(app);
